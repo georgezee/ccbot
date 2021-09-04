a94813b02abd017030f02d7cec79a4336b803452
George Ziady <george@springfisher.com>
2021-09-04 13:13:21 +0200

    Clean up code after adding linting.

--- app.py
+++ app.py
@@ -2,74 +2,82 @@ import os
 import boto3
 import CloudFlare
 import logging
-logging.basicConfig(level=logging.DEBUG)
-
-from CloudFlare.api_v4 import api_v4
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 USERS_TABLE = os.environ['USERS_TABLE']
 client = boto3.client('dynamodb')
 
-# Initializes your app with your bot token and socket mode handler
+# Initializes your app with your bot token
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
     process_before_response=True
 )
 
+
 # Listens to incoming messages that contain "hello"
-# To learn available listener arguments,
-# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+# To learn available listener arguments, visit:
+# https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
 @app.message("hello")
 def message_hello(message, say):
     say(f"Hey there <@{message['user']}>!! :wave:")
     resp = client.put_item(
         TableName=USERS_TABLE,
         Item={
-            'userId': {'S': 'fred01' },
-            'name': {'S': 'Fred D' }
+            'userId': {'S': 'fred01'},
+            'name': {'S': 'Fred D'}
         }
     )
+    say(f"{resp}")
+
+
 @app.message("whois")
 def message_whois(message, say):
-    name = get_user('fred01')
-    say(f"Name is " + str(name))
+    # name = get_user('fred01')
+    name = 'Bob'
+    say(f"Name is {name}")
 
 
-@app.message("clear")
-def message_clear(message, say):
+
+
 @app.command("/clear")
 def message_clear(ack, respond, command):
     ack()
     # Clear the cache for the whole site.
     cloudflare_key = os.environ.get("CF_API_KEY")
     cf = CloudFlare.CloudFlare(token=cloudflare_key)
-    zones = cf.zones.get(params={'per_page':50})
+    zones = cf.zones.get(params={'per_page': 50})
     for zone in zones:
         zone_name = zone['name']
         zone_id = zone['id']
         print(zone_id, zone_name)
         cf.zones.purge_cache.post(
             zone_id,
-            data={'purge_everything':True})
+            data={'purge_everything': True})
     # Inform the user.
-    respond(f"Cache being cleared.!! :broom:")
+    respond("Cache being cleared.!! :broom:")
+
 
 SlackRequestHandler.clear_all_log_handlers()
 logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
+
 
 def get_user(user_id):
     resp = client.get_item(
         TableName=USERS_TABLE,
         Key={
-            'userId': { 'S': user_id }
+            'userId': {'S': user_id}
         }
     )
     item = resp.get('Item')
     if not item:
-        return {'error': 'User does not exist', 'name' : 'Error'}
+        return {'error': 'User does not exist', 'name': 'Error'}
     return item
+
 
 def handler(event, context):
     slack_handler = SlackRequestHandler(app=app)
